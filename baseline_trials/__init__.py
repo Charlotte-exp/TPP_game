6066b85de3a73b472789e3fd0a60e32a6c75f3c3
caroline <caroline.graf@posteo.de>
2024-12-17 11:35:02 +0100

    norm buttons, static norm pages, punishment deduction coins, 2PP give and punish

--- baseline_trials/__init__.py
+++ baseline_trials/__init__.py
@@ -9,13 +9,26 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'baseline_trials'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 6
+    NUM_ROUNDS = 8
 
-    TREATMENTS = ['DG give', 'DG give norm', '3PP give', '3PP give norm', '3PP punish', '3PP punish norm']
-    total_endowment = 30
-    dictator_keeps = 23
-    punishment_points = 10
+    TREATMENTS = ['DG give', 'DG give norm', '3PP give', '3PP punish', '3PP punish norm', '2PP give', '2PP punish', '2PP punish norm']
+    # total_endowment = 30
+    # receiver_endowment = 0
+    # punishment_points = 10
+    # punishment_effectiveness = 3
+    # dictator_keeps = 23  # Should eventually be list: [30, 25, 20, 15]
+    # norm_strategy_dic_gives = 5 # Should eventually be list: [0, 5, 10, 15] : different levels of dictator giving
+    # norm_strategy_dic_gives_binary = 10 # Should eventually be list: ??? [0, 10]? Selfish or less selfish dictator
+    # norm_strategy_punish_norm = 3 # Should eventually be list with 3-4 scenarios: ??? [0, 3, 7, 10]?
+    total_endowment = 12
+    receiver_endowment = 0
+    punishment_points = 4
     punishment_effectiveness = 3
+    dictator_keeps = 8  # Should eventually be list: [30, 25, 20, 15]
+    norm_strategy_dic_gives = 2  # Should eventually be list: [0, 5, 10, 15] : different levels of dictator giving
+    norm_strategy_dic_gives_binary = 3  # Should eventually be list: ??? [0, 10]? Selfish or less selfish dictator
+    norm_strategy_punish_norm = 1  # Should eventually be list with 3-4 scenarios: ??? [0, 3, 7, 10]?
+
 
 
 class Subsession(BaseSubsession):
@@ -39,11 +52,41 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     #dictator_amount = models.IntegerField(min=0, max=10)
-    dictator_amount = models.IntegerField(  # change this to punishment_decision at some point...
+    dic_decision1 = models.IntegerField(
+        initial=0,
+        choices=[
+            [0, f'value 0'], [1, f'value 1'], [2, f'value 2'], [3, f'value 3'], [4, f'value 4'], [5, f'value 5'],
+            [6, f'value 6'], [7, f'value 7'], [8, f'value 8'], [9, f'value 9'], [10, f'value 10'], [11, f'value 11'], [12, f'value 12'], [13, f'value 13'], [14, f'value 14'], [15, f'value 15'],
+            [16, f'value 16'], [17, f'value 17'], [18, f'value 18'], [19, f'value 19'], [20, f'value 20'], [21, f'value 21'], [22, f'value 22'], [23, f'value 23'], [24, f'value 24'], [25, f'value 25'],
+            [26, f'value 26'], [27, f'value 27'], [28, f'value 28'], [29, f'value 29'], [30, f'value 30'],
+        ],
+        verbose_name='[Your decision]',
+        widget=widgets.RadioSelect,
+        # error_messages={'required': 'You must select an option before continuing.'}, # does not display
+    )
+    dic_norm_decision1 = models.IntegerField(
+        initial=0,
+        choices=[
+            [0, f'value 0'], [1, f'value 1'], [2, f'value 2'], [3, f'value 3'], [4, f'value 4'], [5, f'value 5'],
+        ],
+        verbose_name='[Your decision]',
+        widget=widgets.RadioSelect,
+        # error_messages={'required': 'You must select an option before continuing.'}, # does not display
+    )
+    pun_decision1 = models.IntegerField(
         initial=0,
         choices=[
             [0, f'value 0'], [1, f'value 1'], [2, f'value 2'], [3, f'value 3'], [4, f'value 4'], [5, f'value 5'],
             [6, f'value 6'], [7, f'value 7'], [8, f'value 8'], [9, f'value 9'], [10, f'value 10'],
+        ],
+        verbose_name='[Your decision]',
+        widget=widgets.RadioSelect,
+        # error_messages={'required': 'You must select an option before continuing.'}, # does not display
+    )
+    pun_norm_decision1 = models.IntegerField(
+        initial=0,
+        choices=[
+            [0, f'value 0'], [1, f'value 1'], [2, f'value 2'], [3, f'value 3'], [4, f'value 4'], [5, f'value 5'],
         ],
         verbose_name='[Your decision]',
         widget=widgets.RadioSelect,
@@ -56,43 +99,109 @@ class Player(BasePlayer):
 
 
 # PAGES
-class baselinePage(Page):
+class PunishmentPage(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.treatment == "3PP punish" or player.treatment == "2PP punish"
     form_model = 'player'
-    form_fields = ['dictator_amount']
+    form_fields = ['pun_decision1']#, 'decision2']
     @staticmethod
     def vars_for_template(player: Player):
-        print('Generating template variables')
-
-        # Create page per treatment
-        if player.treatment == 'DG give':
-            text = "How much do you give to Person B?"
-        elif player.treatment == 'DG give norm':
-            text = "How socially appropriate is it to give to Person B?"
-        elif player.treatment == '3PP give':
-            text = "How much do you give to Person B?"
-        elif player.treatment == '3PP give norm':
-            text = "How socially appropriate is it to give to Person B?"
-        elif player.treatment == '3PP punish':
-            text = "How much do you punish Person A?"
-        else:
-            text = "How socially appropriate is it to punish Person A?"
-
-        # Load path of treatment image
+        text = "How much do you punish Person A?"
         image = 'baseline/{}.png'.format(player.treatment)
-        print('Generating image path', image)
+        print('Generating image path and round number - 1', image, player.round_number - 1)
 
         return {
             'treatment': player.treatment,
             'treatment_text': text,
             'image': image,
-            'number':player.dictator_amount,
+            'pun_decision1':player.pun_decision1,
+            #'decision2': player.decision2,
             #'receiver_country': player.receiver_country
         }
-
-
     def before_next_page(player: Player, timeout_happened):
-        player.payoff = 10 - player.dictator_amount
+        #player.payoff = 10 - player.pun_decision1
+        player.payoff = C.punishment_points - player.pun_decision1
 
+
+
+class DictatorPage(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.treatment == "3PP give" or player.treatment == "DG give" or player.treatment == "2PP give"
+
+    form_model = 'player'
+    form_fields = ['dic_decision1']  # , 'decision2']
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        text = "How much do you give to Person B?"
+        image = 'baseline/{}.png'.format(player.treatment)
+        print('Generating image path and round number - 1', image, player.round_number - 1)
+
+        return {
+            'treatment': player.treatment,
+            'treatment_text': text,
+            'image': image,
+            'dic_decision1': player.dic_decision1,
+            # 'decision2': player.decision2,
+            # 'receiver_country': player.receiver_country
+        }
+    def before_next_page(player: Player, timeout_happened):
+        player.payoff = C.total_endowment - player.dic_decision1
+
+
+class DictatorNormPage(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.treatment == "3PP give norm" or player.treatment == "DG give norm" or player.treatment == "2PP give norm"
+
+    form_model = 'player'
+    form_fields = ['dic_norm_decision1']
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        text = "How socially acceptable is it to give"
+        image = 'baseline/{}.png'.format(player.treatment)
+        print('Generating image path and round number - 1', image, player.round_number - 1)
+
+        return {
+            'treatment': player.treatment,
+            'treatment_text': text,
+            'image': image,
+            'dic_norm_decision1': player.dic_norm_decision1,
+            # 'decision2': player.decision2,
+            # 'receiver_country': player.receiver_country
+        }
+    # def before_next_page(player: Player, timeout_happened):
+    #     player.payoff = C.total_endowment - player.dic_norm_decision1
+
+class PunishmentNormPage(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.treatment == "3PP punish norm" or player.treatment == "2PP punish norm"
+
+    form_model = 'player'
+    form_fields = ['pun_norm_decision1']
+
+    @staticmethod
+    def vars_for_template(player: Player):
+        text1 = "How socially acceptable is it to punish"
+        text2 = "when Person A gave"
+        image = 'baseline/{}.png'.format(player.treatment)
+        print('Generating image path and round number - 1', image, player.round_number - 1)
+
+        return {
+            'treatment': player.treatment,
+            'treatment_text1': text1,
+            'treatment_text2': text2,
+            'image': image,
+            'pun_norm_decision1': player.pun_norm_decision1,
+            # 'decision2': player.decision2,
+            # 'receiver_country': player.receiver_country
+        }
+    # def before_next_page(player: Player, timeout_happened):
+    #     player.payoff = C.total_endowment - player.dic_norm_decision1
 
 class Results(Page):
     pass
@@ -103,4 +212,5 @@ class instructionPage(Page):
         return player.round_number == 1
 
 #page_sequence = [instructionPage, baselinePage, Results]
-page_sequence = [baselinePage, Results]
+page_sequence = [DictatorPage, PunishmentPage, DictatorNormPage, PunishmentNormPage]
+#page_sequence = [PunishmentPage]
