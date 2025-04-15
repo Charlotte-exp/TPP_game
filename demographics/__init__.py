7847f65d62f083e0a8f0c280d7df0c5027bd496b
Charlotte <rossetti@evolbio.mpg.de>
2025-04-15 15:57:29 +0200

    realtional mobility page for the short scale

--- demographics/__init__.py
+++ demographics/__init__.py
@@ -22,7 +22,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    # Demographics
+    ## Demographics
     age = models.IntegerField(
         verbose_name='What is your age?',
         min=18, max=100
@@ -33,42 +33,56 @@ class Player(BasePlayer):
         verbose_name='What gender do you identify as?',
         widget=widgets.RadioSelect
     )
-
     born = models.StringField(
         choices=['Yes', 'No'],
         verbose_name='Were you born in this country?',
         widget=widgets.RadioSelect
     )
-
     born_parents = models.StringField(
         choices=['Yes', 'No'],
         verbose_name='Were you born in this country?',
         widget=widgets.RadioSelect
     )
-
     how_long = models.StringField(
         choices=['less than 1 year', '1-5 years', '5-10 years', 'more than 10 years'],
         verbose_name='How long have you lived in this country?',
         widget=widgets.RadioSelect
     )
-
     income_ladder = models.IntegerField(
         choices=[i for i in range(1, 11)],
         blank=True,
         label="Where would you place yourself on this ladder?"
     )
-
     education = models.StringField(
         choices=['No formal education', 'Compulsory school', 'Post-secondary education',
                  'Undergraduate degree', 'Postgraduate degree', 'Prefer not to say'],
         verbose_name='What is the highest level of education you have completed?',
-        widget=widgets.RadioSelect)
-
+        widget=widgets.RadioSelect
+    )
     rural = models.StringField(
-        choices=["less than 10.000 inhabitants", 'between 10.000 and 250.000 inhabitants', 'between 250.000 and 1 million inhabitants',
-                 'More than 1 million inhabitants'],
+        choices=["less than 10.000 inhabitants", 'between 10.000 and 250.000 inhabitants',
+                 'between 250.000 and 1 million inhabitants', 'More than 1 million inhabitants'],
         verbose_name='How large was the place where grew up?',
-        widget=widgets.RadioSelect)
+        widget=widgets.RadioSelect
+    )
+
+    ## Relational Mobility
+    q1 = models.StringField(
+        choices=[
+            [0, 'Strongly disagree'], [1, 'Disagree'], [2, 'Slightly disagree'],
+            [3, 'Slightly agree'], [4, 'Agree'], [5, 'Strongly agree'],
+        ],
+        verbose_name='How large was the place where grew up?',
+        widget=widgets.RadioSelect
+    )
+    q2 = models.StringField(
+        choices=[
+            [0, 'Strongly disagree'], [1, 'Disagree'], [2, 'Slightly disagree'],
+            [3, 'Slightly agree'], [4, 'Agree'], [5, 'Strongly agree'],
+        ],
+        verbose_name='How large was the place where grew up?',
+        widget=widgets.RadioSelect
+    )
 
     comment_box = models.LongStringField(
         verbose_name=''
@@ -76,6 +90,14 @@ class Player(BasePlayer):
 
 
 ########### PAGES ############
+
+class Demographics(Page):
+    form_model = 'player'
+    form_fields = ['age', 'gender','born','born_parents','how_long', 'education', 'rural']
+
+    def before_next_page(player: Player, timeout_happened):
+        participant = player.participant
+        #participant.progress += 1
 
 class Ladder(Page):
     form_model = 'player'
@@ -91,25 +113,32 @@ class Ladder(Page):
         participant = player.participant
         #participant.progress += 1
 
-
-class Demographics(Page):
+class RelationalMobility(Page):
     form_model = 'player'
-    form_fields = ['age', 'gender','born','born_parents','how_long', 'education', 'rural']
+    form_fields = ['q1', 'q2']
 
     def before_next_page(player: Player, timeout_happened):
         participant = player.participant
         #participant.progress += 1
-
 
 class ResultsWaitPage(WaitPage):
     pass
 
 
 class Payment(Page):
-    pass
+
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.round_number == C.NUM_ROUNDS
+
+    def vars_for_template(player: Player):
+        return {
+            'participation_fee': player.session.config['participation_fee'],
+        }
 
 
 page_sequence = [Ladder,
+                 RelationalMobility,
                  Demographics,
                  #ResultsWaitPage,
                  Payment]
