57b9e60b444d58d29cde8db72a196ef1565bf40f
Charlotte <rossetti@evolbio.mpg.de>
2025-04-28 14:11:08 +0200

    e

--- crowding_out/__init__.py
+++ crowding_out/__init__.py
@@ -46,7 +46,12 @@ def creating_session(subsession): # Just for testing treatment allocation, will 
         participant = player.participant
         participant.current_country = "Switzerland"
         participant.crowding_out_button_pos = random.choice([True, False])
+
+        ''' ONLY WHEN TESTING APP ON ITS OWN'''
+        participant.progress = 1
+
         print('set crowding_out_button_pos', participant.crowding_out_button_pos)
+
 
 
 def get_local_red_cross_info(country_name):
@@ -179,7 +184,12 @@ class CrowdingInOutPage(Page):
             local_red_cross = local_red_cross,
             image_red_cross_local = image_red_cross_local,
             current_country=current_country,
+            total_pages=player.session.config['total_pages'],
         )
+
+    def before_next_page(player: Player, timeout_happened):
+        participant = player.participant
+        participant.progress += 1
 
 class DescriptiveNormPage(Page):
 
@@ -216,7 +226,12 @@ class DescriptiveNormPage(Page):
             text1 = text1,
             text2 = text2,
             current_country=current_country,
+            total_pages=player.session.config['total_pages'],
         )
+
+    def before_next_page(player: Player, timeout_happened):
+        participant = player.participant
+        participant.progress += 1
 
 class ConditionalCoopPage(Page):
 
@@ -298,7 +313,12 @@ class ConditionalCoopPage(Page):
             local_red_cross=local_red_cross,
             image_red_cross_local=image_red_cross_local,
             current_country=current_country,
+            total_pages=player.session.config['total_pages'],
         )
+
+    def before_next_page(player: Player, timeout_happened):
+        participant = player.participant
+        participant.progress += 1
 
 
 page_sequence = [CrowdingInOutPage,
