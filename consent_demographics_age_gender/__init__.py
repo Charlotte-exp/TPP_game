2e55cb834074d0b7fdebf82f3a7c4bc9299cd6c9
caroline <caroline.graf@posteo.de>
2025-12-23 12:47:51 +0100

    ua updates

--- consent_demographics_age_gender/__init__.py
+++ consent_demographics_age_gender/__init__.py
@@ -435,7 +435,7 @@ class Demographics_age_gender(Page):
             # Extra counts to check how many people started survey in last 25 minutes
             NOW = time.time()
             WINDOW_SECONDS = 25 * 60
-            MAX_ACTIVE_STARTERS = 50
+            MAX_ACTIVE_STARTERS = 35
             recent_starters = 0
 
             # Get all players in the session
